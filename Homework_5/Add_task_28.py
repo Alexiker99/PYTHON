c958f29f3f9de4d8872ff3f688f722bcb06292ae
Alexiker99 <alexiker99@gmail.com>
2023-04-01 00:10:35 +0400

    Выполнили Доп.Задание №28

--- Homework_5/Add_task_28.py
+++ Homework_5/Add_task_28.py
@@ -6,3 +6,32 @@
 # Пример:
 # 
 # 2 2 <- 4
+
+def get_int_from_user(message):
+    try:
+        n = int(input(message))
+    except ValueError:
+        return
+    else:
+        return n
+
+
+def get_sum(a, b):
+    if b == 0:
+        return a
+    if b > 0:
+        return get_sum(a + 1, b - 1)
+    return get_sum(a - 1, b + 1)
+
+
+def start_app():
+    a = get_int_from_user('Введите 1 слагаемое: ')
+    b = get_int_from_user('Введите 2 слагаемое: ')
+    if a is not None and b is not None:
+        print(f'Результат a + b = {get_sum(a, b)}')
+    else:
+        print('Ошибка, повторите')
+
+
+if __name__ == "__main__":
+    start_app()
