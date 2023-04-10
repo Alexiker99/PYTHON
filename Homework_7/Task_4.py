c3e8a95548395796b04f0d86a2967227d8dc2733
Alexiker99 <alexiker99@gmail.com>
2023-04-10 11:47:34 +0400

    Выполнено Задание №4

--- Homework_7/Task_4.py
+++ Homework_7/Task_4.py
@@ -24,3 +24,51 @@
 # Результатом сложения должна быть новая матрица.
 # Подсказка: сложение элементов матриц выполнять поэлементно — 
 # первый элемент первой строки первой матрицы складываем с первым элементом первой строки второй матрицы и т.д.
+
+class Matrix:
+    def __init__(self, user_list):
+        self.data = user_list
+        self.rows = len(user_list)
+        self.cols = len(user_list[0])
+
+    def __str__(self):
+        res = ""
+        for row in self.data:
+            for el in row:
+                res += f"{el} "
+            res += "\n"
+        return res
+
+    def __to_list(self):
+        res = []
+        for row in self.data:
+            for el in row:
+                res.append(el)
+        return res
+
+    def __add__(self, other):
+        arr1 = self.__to_list()
+        arr2 = other.__to_list()
+        res_rows = self.rows if self.rows > other.rows else other.rows
+        res_cols = self.cols if self.cols > other.cols else other.cols
+        arr_sum = []
+        res_matrix = []
+        count = 0
+        for i in range(len(arr1)):
+            try:
+                arr_sum.append(arr1[i] + arr2[i])
+            except IndexError:
+                arr_sum.append(arr1[i])
+        for rows in range(res_rows):
+            inner = []
+            for cols in range(res_cols):
+                inner.append(arr_sum[count])
+                count += 1
+            res_matrix.append(inner)
+        return Matrix(res_matrix)
+
+
+obj = Matrix([[31, 22], [37, 43], [51, 68]])
+obj2 = Matrix([[31, 22], [37, 43]])
+
+print(obj + obj2)
