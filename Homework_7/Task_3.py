301b525b5929356f6a144c19ada466253428b1db
Alexiker99 <alexiker99@gmail.com>
2023-04-10 11:47:00 +0400

    Выполнено Задание №3

--- Homework_7/Task_3.py
+++ Homework_7/Task_3.py
@@ -13,3 +13,31 @@
 # 
 # П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку str
 # str(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
+
+class Worker:
+    _income = {"wage": 0,
+               "bonus": 0}
+
+    def __init__(self, name, surname, position):
+        self.name = name
+        self.surname = surname
+        self.position = position
+
+
+class Position(Worker):
+    def __init__(self, name, surname, position, income):
+        super().__init__(name, surname, position)
+        self._income = income
+
+    def get_full_name(self):
+        return f"{self.name} {self.surname}"
+
+    def get_total_income(self):
+        return self._income["wage"] + self._income["bonus"]
+
+    def __str__(self):
+        return self.get_full_name() + "\n" + str(self.get_total_income())
+
+
+obj = Position("Alexandr", "Pertsev", "S.worker", {'wage': 2000, 'bonus': 500})
+print(obj)
