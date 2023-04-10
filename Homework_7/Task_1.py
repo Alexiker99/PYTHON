24dbf2533a1b131c70c12dc27a998a12dc3e4f93
Alexiker99 <alexiker99@gmail.com>
2023-04-10 11:45:50 +0400

    Выполнено Задание №1

--- Homework_7/Task_1.py
+++ Homework_7/Task_1.py
@@ -13,3 +13,22 @@
 # в указанном порядке (красный, желтый, зеленый).
 # 
 # Проверить работу примера, создав экземпляр и вызвав описанный метод.
+
+from time import sleep
+
+
+class TrafficLight:
+    __color = ['red', 'yellow', 'green'];
+
+    def running(self):
+        print(self.__color[0])
+        sleep(7)
+        print(self.__color[1])
+        sleep(2)
+        print(self.__color[2])
+        sleep(5)
+        self.running()
+
+
+obj = TrafficLight()
+obj.running()
