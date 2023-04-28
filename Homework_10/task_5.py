a7a08c737a04ce0c6e6037fe16b9b923423b218e
Alexiker99 <alexiker99@gmail.com>
2023-04-28 11:25:04 +0400

    Выполнено Задание №5

--- Homework_10/task_5.py
+++ Homework_10/task_5.py
@@ -6,3 +6,19 @@
 # Подсказки:
 #--- используйте модуль chardet, иначе задание не засчитается!!!
 
+import subprocess
+import chardet
+
+web_address_list = ['2ch.hk', 'playground.com']
+
+def ping_website(adresses):
+    for address in adresses:
+        print(f'Пинг сайта: {address}')
+        command = ['ping']
+        command.append(address)
+        res_ping = subprocess.Popen(command, stdout=subprocess.PIPE)
+        for line in res_ping.stdout:
+            line_encode = chardet.detect(line)
+            print(line.decode(line_encode['encoding']))
+
+ping_website(web_address_list)
