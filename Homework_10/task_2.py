d7b8682b78af66549e10c5a554c6f0715273eb4a
Alexiker99 <alexiker99@gmail.com>
2023-04-28 11:22:05 +0400

    Выполнено Задание №2

--- Homework_10/task_2.py
+++ Homework_10/task_2.py
@@ -9,3 +9,8 @@
 # --- b'class' - используйте маркировку b''
 # --- используйте списки и циклы, не дублируйте функции
 
+words_list = ['class', 'function', 'method']
+byte_list = [bytes(word, 'utf-8') for word in words_list]
+
+for word in byte_list:
+    print(f'{word}.Тип данных: {type(word)} Длина переменной: {len(word)}')
